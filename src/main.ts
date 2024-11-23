c6f16d6d47202ed53040970adde16ee995c7a896
Helamã Ewerton <helamaewerton@gmail.com>
2024-11-22 22:49:48 -0700

    feat: set global API prefix and enable CORS with frontend URL configuration

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,17 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(3000);
+  app.setGlobalPrefix('api'); // Set the global prefix
+  const url = process.env.FRONTEND_URL;
+  console.log('FRONTEND_URL', url);
+  // Enable CORS
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type, Accept, Authorization'],
+    credentials: true,
+  });
+
+  await app.listen(3001);
 }
 bootstrap();
