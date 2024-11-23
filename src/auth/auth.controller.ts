07e6f3abe360911007a21b220d13be247a82c942
Helamã Ewerton <helamaewerton@gmail.com>
2024-11-23 02:07:46 -0700

    feat: enhance user validation and authentication; return access token on successful login and define UserAuth type

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,6 +7,7 @@ import {
   Res,
   UseGuards,
 } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
 import { Request, Response } from 'express';
 import { LoginUserDto } from 'src/user/dto/login-user.dto';
@@ -14,7 +15,10 @@ import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   @Post('login')
   async login(@Body() loginUserDto: LoginUserDto, @Res() res: Response) {
@@ -32,5 +36,17 @@ export class AuthController {
   @UseGuards(AuthGuard('jwt'))
   async verify(@Req() req: Request) {
     return { message: 'User is logged in', user: req.user };
+  }
+
+  @Post('verify-token')
+  async verifyToken(@Body('token') token: string) {
+    try {
+      const decoded = this.jwtService.verify(token.replace('Bearer ', ''), {
+        secret: process.env.JWT_SECRET,
+      });
+      return { valid: true, decoded };
+    } catch (error) {
+      return { valid: false, error: error.message };
+    }
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,6 @@
 // src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
@@ -16,7 +16,7 @@ import { JwtStrategy } from './jwt.strategy';
       signOptions: { expiresIn: '60m' },
     }),
   ],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, JwtService],
   controllers: [AuthController],
 })
 export class AuthModule {}
